f7885cad17b857bc47284492b036c2ec09b66af7
samiksha Shende <shende.samiksha@rediffmail.com>
2020-11-05 12:40:04 +0530

    Creating method to take input from user

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,5 +1,7 @@
 package tictactoe;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
 
 	// UC1 - creating method to initialize indices with blank space
@@ -11,11 +13,35 @@ public class TicTacToeGame {
 		return board;
 	}
 
+	// UC2 - Creating method to take the input from user
+	static void user_Input() {
+
+		char computer = 'O';
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Enter the input X/O");
+
+		char userInput = input.next().charAt(0);
+
+		if (userInput == 'X') {
+			System.out.println("player = " + computer + "   computer = " + userInput);
+		} else if (userInput == 'O') {
+			computer = 'X';
+			System.out.println("player = " + computer + "  computer = " + userInput);
+		} else {
+			System.out.println("Invalid input");
+		}
+
+		input.close();
+	}
+
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to TicTacToe Game");
 
 		createBoard();
 
+		user_Input();
 	}
 }
